eb49c0b486530fde1e287acc122eec724bda16d3
akv17 <temakep@list.ru>
2023-08-04 22:25:13 +0300

    wip: muldim softmax via numpy directly

--- tensorgrad/ops/nn.py
+++ tensorgrad/ops/nn.py
@@ -61,39 +61,102 @@ class Softmax(Op):
         return self.out
 
     def backward(self):
-        # here we have to compute full jacobian as each output depends on each input.
-        # df/di when i != j: -softmax(j) * softmax(i)
-        # df/di grad when i == j: softmax(i) * (1.0 - softmax(i))
+        # brief explanation of the implementation:
+        # - softmax operates only on vectors.
+        # - no matter how many dimensions input has, softmax output may always be seen as a collection of `k` softmax vectors.
+        # - for inputs of `ndim>1` each resulting softmax vector is independent from any other.
+        # - the same holds for the gradients.
+        # - leveraging that we arrange computations around vectors.
+
+        # 1. use softmax forward output tensor.
+        # 2. swap `self.dim` dim with the last dim, so `self.dim` is now the last dim.
+        # 3. flatten tensor to shape [-1, sizeOf(self.dim)] -> a collection of `k` vectors along `self.dim` dim.
+        # 4. cast upstream grad tensor to the same shape (originally it has the same shape as softmax forward output).
+        # 5. compute local grad as a collection of `k` jacobians -- separate jacobian for each of the `k` softmax vectors.
+        #     for each of `k` vectors:
+        #         5.1. number of inputs `n` is equal to the size of `self.dim`.
+        #         5.2. number of outputs is equal to the number of inputs.
+        #         5.3. thus jacobian has shape [n, n].
+        #         5.4. element `i,j` is computed as: -softmax(j) * softmax(i)
+        #         5.5. element `i,i` is computed as: softmax(i) * (1.0 - softmax(i))
+        #         5.6  compute 5.4 as outer product.
+        #         5.7. this settles down the jacobian.
+        #         5.8  compute 5.5 and fill the jacobian diagonal with the result.
+        # 6. now we have collection of `k` jacobians where `k` is the number of vectors along `self.dim`
+        # 7. compute downstream grad by multiplying each jacobian with the upstream grad from step 4.
+        # 8. cast downstream grad to the shape of softmax inputs (cast back to original shape from step 1)
 
         if not self.x.requires_grad:
             return
-
+        
         import numpy as np
+        # step: 1
         softmax = self.out.data.data
         shape = softmax.shape
-        dims = list(range(len(shape)))
         dim = self.dim
-        t = dims.copy()
-        t[dim] = dims[-1]
-        t[-1] = dim
-        softmax_t = np.transpose(softmax, t)
-        t_shape = softmax_t.shape
+        dims = list(range(len(shape)))
+        
+        # step: 2
+        transpose_dims = dims.copy()
+        transpose_dims[dim] = dims[-1]
+        transpose_dims[-1] = dim
+
+        # step: 3
+        softmax_t = np.transpose(softmax, transpose_dims)
+        transpose_shape = softmax_t.shape
         softmax_t = softmax_t.reshape(-1, softmax_t.shape[-1])
-        grad_t = np.transpose(self.out.grad.data, t)
-        grad_t = grad_t.reshape(-1, grad_t.shape[-1])
-        lgrads = []
-        for i in range(len(softmax_t)):
-            si = softmax_t[i]
-            gi = grad_t[i]
-            jacob = -(si.reshape(-1, 1)).dot(si.reshape(1, -1))
-            d_ii = si * (1.0 - si)
-            np.fill_diagonal(jacob, d_ii)
-            gi = jacob.dot(gi)
-            lgrads.append(gi)
-        lgrads = np.array(lgrads)
-        lgrads = lgrads.reshape(t_shape)
-        lgrads = np.transpose(lgrads, t)
-        self.x.grad.data += lgrads
+        
+        # step: 4
+        u_grad_t = np.transpose(self.out.grad.data, transpose_dims)
+        u_grad_t = u_grad_t.reshape(-1, u_grad_t.shape[-1])
+
+        # step: 5
+        # step: 5.6
+        outer_a = np.expand_dims(softmax_t, -1)
+        outer_b = np.expand_dims(softmax_t, 1)
+        outer = np.matmul(-outer_a, outer_b)
+
+        # step: 5.8
+        d_ii = softmax_t * (1.0 - softmax_t)
+        outer[:, range(shape[dim]), range(shape[dim])] = d_ii
+
+        # step: 7
+        d_grad = np.matmul(outer, np.expand_dims(u_grad_t, -1))
+        
+        # step:8 
+        d_grad = d_grad.reshape(transpose_shape)
+        d_grad = np.transpose(d_grad, transpose_dims)
+
+        self.x.grad.data += d_grad
+
+
+
+        # import numpy as np
+        # softmax = self.out.data.data
+        # shape = softmax.shape
+        # dims = list(range(len(shape)))
+        # dim = self.dim
+        # t = dims.copy()
+        # t[dim] = dims[-1]
+        # t[-1] = dim
+        # softmax_t = np.transpose(softmax, t)
+        # t_shape = softmax_t.shape
+        # softmax_t = softmax_t.reshape(-1, softmax_t.shape[-1])
+        # grad_t = np.transpose(self.out.grad.data, t)
+        # grad_t = grad_t.reshape(-1, grad_t.shape[-1])
+        # lgrads = []
+        # for i in range(len(softmax_t)):
+        #     si = softmax_t[i]
+        #     gi = grad_t[i]
+        #     jacob = -(si.reshape(-1, 1)).dot(si.reshape(1, -1))
+        #     d_ii = si * (1.0 - si)
+        #     np.fill_diagonal(jacob, d_ii)
+        #     gi = jacob.dot(gi)
+        #     lgrads.append(gi)
+        # lgrads = np.array(lgrads)
+        # lgrads = lgrads.reshape(t_shape)
+        # lgrads = np.transpose(lgrads, t)
+        # self.x.grad.data += lgrads
         
 
 
--- tests/ops/test_nn_ops.py
+++ tests/ops/test_nn_ops.py
@@ -18,16 +18,16 @@ SHAPES_TESTED = (
     (10,),
     (100,),
     (1000,),
-    (10, 10),
-    (100, 100),
-    (10, 100, 100),
+    (5, 10),
+    (50, 100),
+    (5, 50, 100),
 )
 BACKENDS_TESTED = (
     BACKEND.NUMPY,
 )
 DTYPES_TESTED = (
     DTYPE.FLOAT32,
-    DTYPE.FLOAT64,
+    # DTYPE.FLOAT64,
 )
 
 CASES = generate_cases(OPS_TESTED, SHAPES_TESTED, BACKENDS_TESTED, DTYPES_TESTED)
@@ -35,8 +35,26 @@ CASES = generate_cases(OPS_TESTED, SHAPES_TESTED, BACKENDS_TESTED, DTYPES_TESTED
 
 class TestBinaryOps(unittest.TestCase):
 
-    @parameterized.expand(CASES)
-    def test(self, op, shape, backend, dtype):
+    @parameterized.expand(
+        generate_cases(
+            (
+                OP.RELU,
+                OP.SIGMOID
+            ),
+            (
+                (1,),
+                (10,),
+                (100,),
+                (1000,),
+                (5, 10),
+                (50, 100),
+                (5, 50, 100),
+            ),
+            BACKENDS_TESTED,
+            DTYPES_TESTED
+        )
+    )
+    def test_activations(self, op, shape, backend, dtype):
         name = f'{op}::{shape}::{backend}::{dtype}'
         method = self._op_to_method(op)
 
@@ -49,19 +67,79 @@ class TestBinaryOps(unittest.TestCase):
         _x = _x.tolist()
     
         x = Tensor(_x, name='x', dtype=dtype, backend=backend, requires_grad=True)
-        y = getattr(x, method)().exp().sum()
+        y = getattr(x, method)().sum()
         y.backward()
 
         tdtype = getattr(torch, dtype.value)
         tx = torch.tensor(_x, dtype=tdtype, requires_grad=True)
-        ty = getattr(tx, method)().exp().sum()
+        ty = getattr(tx, method)().sum()
         ty.backward()
 
         self._check_tensors(ty, y, msg=f'{name}@forward')
         self._check_tensors(tx.grad, x.grad, msg=f'{name}@x_grad')
+
+    @parameterized.expand(
+        generate_cases(
+            [0],
+            [
+                (1,),
+                (10,),
+                (100,),
+                (1000,),
+            ],
+            BACKENDS_TESTED,
+            DTYPES_TESTED
+        )
+        +
+        generate_cases(
+            [0, 1],
+            [
+                (2, 3),
+                (200, 300),
+            ],
+            BACKENDS_TESTED,
+            DTYPES_TESTED
+        )
+        +
+        generate_cases(
+            [0, 1, 2],
+            [
+                (2, 3, 4),
+                (20, 30, 40),
+            ],
+            BACKENDS_TESTED,
+            DTYPES_TESTED
+        )
+        +
+        generate_cases(
+            [0, 1, 2, 3],
+            [
+                (2, 3, 4, 5),
+                (10, 20, 30, 40),
+            ],
+            BACKENDS_TESTED,
+            DTYPES_TESTED
+        )
+    )
+    def test_softmax(self, dim, shape, backend, dtype):
+        name = f'softmax::{dim}::{shape}::{backend}::{dtype}'
+
+        _x = np.random.uniform(-1.0, 1.0, size=shape).tolist()
     
+        x = Tensor(_x, name='x', dtype=dtype, backend=backend, requires_grad=True)
+        y = x.softmax(dim).log().sum()
+        y.backward()
+
+        tdtype = getattr(torch, dtype.value)
+        tx = torch.tensor(_x, dtype=tdtype, requires_grad=True)
+        ty = tx.softmax(dim).log().sum()
+        ty.backward()
+
+        self._check_tensors(ty, y, msg=f'{name}@forward')
+        self._check_tensors(tx.grad, x.grad, msg=f'{name}@x_grad')
+
     def _check_tensors(self, a, b, tol=1e-5, msg=''):
-        self.assertTrue(check_tensors(a.tolist(), b.tolist(), tol=tol), msg=msg)
+        self.assertTrue(check_tensors(a.tolist(), b.tolist(), tol=tol, show_diff=False), msg=msg)
 
     def _op_to_method(self, op):
         return {
