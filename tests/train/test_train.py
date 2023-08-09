d8d95474c63e4ac967895013dcddbfe6ada26360
akv17 <temakep@list.ru>
2023-08-09 23:49:20 +0300

    add training tests

--- tests/train/test_train.py
+++ tests/train/test_train.py
@@ -4,74 +4,61 @@ import unittest
 
 import numpy as np
 
-
 from tests.util import require_torch, check_tensors
 import tensorgrad
 
 torch = require_torch()
+torch.manual_seed(0)
 
 
 class TestTraining(unittest.TestCase):
 
     def test_mlp_classifier(self):
-        torch.manual_seed(0)
-        with open(os.path.join('tests', 'train', 'data', 'reg.pkl'), 'rb') as f:
+        with open(os.path.join('tests', 'train', 'data', 'clf.pkl'), 'rb') as f:
             _x, _y = pickle.load(f)
         _x = np.array(_x)
         _y = np.array(_y)
         
         num_features = _x.shape[-1]
         num_classes = len(set(_y))
-        num_epochs = 1
+        num_epochs = 10
         batch_size = 4
         lr = 0.01
-        momentum = 0.0
+        momentum = 0.9
 
         tx = torch.tensor(_x, requires_grad=False).float()
-        ty = torch.tensor(_y, requires_grad=False).float()
+        ty = torch.tensor(_y, requires_grad=False).long()
         tmodel = torch.nn.Sequential(
-            torch.nn.Linear(num_features, 1),
-            # torch.nn.Identity(),
-            # torch.nn.Linear(32, 16),
-            # torch.nn.Identity(),
-            # torch.nn.Linear(16, 8),
-            # torch.nn.Identity(),
-            # torch.nn.Linear(8, 4),
-            # torch.nn.Identity(),
-            # torch.nn.Linear(4, num_classes),
+            torch.nn.Linear(num_features, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 16),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(16, 8),
+            torch.nn.ReLU(),
+            torch.nn.Linear(8, 4),
+            torch.nn.Sigmoid(),
+            torch.nn.Linear(4, num_classes),
         )
         tlayers = tmodel[::2]
-        tloss = torch.nn.MSELoss()
+        tloss = torch.nn.CrossEntropyLoss()
         toptim = torch.optim.SGD(tmodel.parameters(), lr=lr, momentum=momentum)
 
         x = tensorgrad.Tensor(_x, name='x', dtype=tensorgrad.DTYPE.FLOAT32, requires_grad=False)
-        y = tensorgrad.Tensor(_y, dtype=tensorgrad.DTYPE.FLOAT32, requires_grad=False)
+        y = tensorgrad.Tensor(_y, name='y', dtype=tensorgrad.DTYPE.INT64, requires_grad=False)
         model = tensorgrad.nn.Sequential(
-            tensorgrad.nn.Linear(num_features, 1).initialize(weight=tmodel[0].weight.data.numpy(), bias=tmodel[0].bias.data.numpy()),
-            # tensorgrad.nn.Identity(),
-            # tensorgrad.nn.Linear(32, 16).initialize(weight=tmodel[2].weight.detach().numpy(), bias=tmodel[2].bias.detach().numpy()),
-            # tensorgrad.nn.Identity(),
-            # tensorgrad.nn.Linear(16, 8).initialize(weight=tmodel[4].weight.detach().numpy(), bias=tmodel[4].bias.detach().numpy()),
-            # tensorgrad.nn.Identity(),
-            # tensorgrad.nn.Linear(8, 4).initialize(weight=tmodel[6].weight.detach().numpy(), bias=tmodel[6].bias.detach().numpy()),
-            # tensorgrad.nn.Identity(),
-            # tensorgrad.nn.Linear(4, num_classes).initialize(weight=tmodel[8].weight.detach().numpy(), bias=tmodel[8].bias.detach().numpy()),
+            tensorgrad.nn.Linear(num_features, 32).initialize(weight=tmodel[0].weight.data.tolist(), bias=tmodel[0].bias.data.tolist()),
+            tensorgrad.nn.ReLU(),
+            tensorgrad.nn.Linear(32, 16).initialize(weight=tmodel[2].weight.detach().numpy(), bias=tmodel[2].bias.detach().numpy()),
+            tensorgrad.nn.Sigmoid(),
+            tensorgrad.nn.Linear(16, 8).initialize(weight=tmodel[4].weight.detach().numpy(), bias=tmodel[4].bias.detach().numpy()),
+            tensorgrad.nn.ReLU(),
+            tensorgrad.nn.Linear(8, 4).initialize(weight=tmodel[6].weight.detach().numpy(), bias=tmodel[6].bias.detach().numpy()),
+            tensorgrad.nn.Sigmoid(),
+            tensorgrad.nn.Linear(4, num_classes).initialize(weight=tmodel[8].weight.detach().numpy(), bias=tmodel[8].bias.detach().numpy()),
         )
         layers = model[::2]
-        loss = tensorgrad.nn.MSELoss()
+        loss = tensorgrad.nn.CrossEntropyLoss()
         optim = tensorgrad.nn.SGD(model.parameters(), lr=lr, momentum=momentum)
-
-
-        # for tm, m in zip(tmodel, model):
-        #     print('torch')
-        #     for tp in tm.parameters():
-        #         print(tp.shape, tp.sum())
-        #     print('tensorgrad')
-        #     for p in m.parameters():
-        #         print(p.shape, p.sum().data)
-        #     print()
-        #     print()
-        # exit()
 
         print()
         step = 0
@@ -83,39 +70,26 @@ class TestTraining(unittest.TestCase):
                 txb = tx[i:i+batch_size]
                 tyb = ty[i:i+batch_size]
                 toptim.zero_grad()
-                tout = tmodel(txb).reshape(-1)
+                tout = tmodel(txb)
                 tlossv = tloss(tout, tyb)
                 tlossv.backward()
                 toptim.step()
 
                 xb = x[i:i+batch_size]
-                xb.requires_grad = False
-                xb.name = 'xb'
                 yb = y[i:i+batch_size]
-                yb.requires_grad = False
                 optim.zero_grad()
-                out = model(xb).reshape(-1)
+                out = model(xb)
                 lossv = loss(out, yb)
                 lossv.backward()
                 optim.step()
-                xb.grad = xb.grad.zeros_like()
 
-                print(f'epoch: {epoch} step: {step} torch_loss: {tlossv.item():.8f} tensorgrad_loss: {lossv.item():.8f} {abs(tlossv.item() - lossv.item())}')
-                # self.assertTrue(check_tensors(tlossv, lossv), msg=f'forward@step{step}')
+                print(f'epoch: {epoch} step: {step} torch_loss: {tlossv.item():.8f} tensorgrad_loss: {lossv.item():.8f}')
+                self.assertTrue(check_tensors(tlossv, lossv), msg=f'forward@step{step}')
 
                 for tlayer, layer in zip(tlayers, layers):
-                    tlw = tlayer.weight
-                    tlb = tlayer.weight
-                    lw = layer.weight
-                    lb = layer.bias
-                    # self.assertTrue(check_tensors(tlw, lw, tol=1e-5, show_diff=True), msg=f'w_grad@{layer.weight.shape}')
-                
-                break
-
-
-        
-    
-
-        # tmodel = torch.nn.Sequential(
-        #     torch.nn.Linear()
-        # )
+                    tlw = tlayer.weight.grad
+                    tlb = tlayer.bias.grad
+                    lw = layer.weight.grad
+                    lb = layer.bias.grad
+                    self.assertTrue(check_tensors(tlw, lw, tol=1e-5, show_diff=True), msg=f'w_grad@{layer.weight.shape}')
+                    self.assertTrue(check_tensors(tlb, lb, tol=1e-5, show_diff=True), msg=f'b_grad@{layer.bias.shape}')
