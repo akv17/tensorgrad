2caa1021f153793c3b14d3a2754c0618467be75f
akv17 <temakep@list.ru>
2023-08-10 18:48:58 +0300

    add adam optim

--- tensorgrad/nn/optim.py
+++ tensorgrad/nn/optim.py
@@ -24,3 +24,36 @@ class SGD:
                 self._momentum_buffer[pi] = b
                 g = b
             p.data -= self.lr * g
+
+
+class Adam:
+    
+    def __init__(self, parameters, lr=0.001, betas=(0.9, 0.999), eps=1e-8, name=None):
+        self.parameters = parameters
+        self.lr = lr
+        self.betas = betas
+        self.beta1, self.beta2 = self.betas
+        self.eps = eps
+        self.name = name or f'adam@{id(self)}'
+        self._step = 0
+        self._moment1_buffer = [None] * len(self.parameters)
+        self._moment2_buffer = [None] * len(self.parameters)
+
+    def zero_grad(self):
+        for p in self.parameters:
+            p.grad = p.grad.zeros_like()
+
+    def step(self):
+        self._step += 1
+        for pi, p in enumerate(self.parameters):
+            g = p.grad
+            m_prev = g.zeros_like() if self._step == 1 else self._moment1_buffer[pi]
+            m = self.beta1 * m_prev + (1.0 - self.beta1) * g
+            self._moment1_buffer[pi] = m
+            v_prev = g.zeros_like() if self._step == 1 else self._moment2_buffer[pi]
+            v = self.beta2 * v_prev + (1.0 - self.beta2) * (g ** 2)
+            self._moment2_buffer[pi] = v
+            m = m / (1.0 - self.beta1 ** (self._step))
+            v = v / (1.0 - self.beta2 ** (self._step))
+            upd = (self.lr * m) / (v.sqrt() + self.eps)
+            p.data -= upd
--- tests/nn/test_optim.py
+++ tests/nn/test_optim.py
@@ -16,14 +16,10 @@ class TestOptim(unittest.TestCase):
 
     @parameterized.expand(
         generate_cases(
-            # [(4,), (128), (16, 32), (8, 16, 32)],
-            # [1, 4, 16],
-            # [0.1, 0.001, 1e-5],
-            # [None, 0.9],
-            [(4,)],
-            [4],
-            [0.1],
-            [None],
+            [(4,), (128), (16, 32), (8, 16, 32)],
+            [1, 4, 16],
+            [0.1, 0.001, 1e-5],
+            [None, 0.9],
             BACKENDS_TESTED,
             DTYPES_TESTED
         )
@@ -39,22 +35,56 @@ class TestOptim(unittest.TestCase):
         toptim = torch.optim.SGD([tx], lr=lr, momentum=momentum or 0.0)
 
         x = tensorgrad.Tensor(tx.detach().numpy(), name='x', backend=backend, dtype=dtype, requires_grad=True)
-
-        x = tensorgrad.nn.Parameter(x)
         optim = tensorgrad.nn.SGD([x], lr=lr, momentum=momentum)
 
         for _ in range(num_steps):
             toptim.zero_grad()
-            tf = tx.softmax(-1).mean()
+            tf = tx.exp().mean()
             tf.backward()
             toptim.step()
 
             optim.zero_grad()
-            f = x.softmax(-1).mean()
+            f = x.exp().mean()
             f.backward()
             optim.step()
+            
+            self.assertTrue(check_tensors(tf, f, tol=1e-5, show_diff=True), msg=f'forward@{name}')
+            self.assertTrue(check_tensors(tx.grad, x.grad, tol=1e-5, show_diff=True), msg=f'x_grad@{name}')
 
-            print(tx.grad, x.grad)
+    @parameterized.expand(
+        generate_cases(
+            [(4,), (128), (16, 32), (8, 16, 32)],
+            [1, 4, 16],
+            [0.001],
+            [(0.9, 0.999)],
+            [1e-8],
+            BACKENDS_TESTED,
+            DTYPES_TESTED
+        )
+    )
+    def test_adam(self, shape, num_steps, lr, betas, eps, backend, dtype):
+        name = f'{shape}::{num_steps}::{lr}::{betas}::{eps}::{backend}::{dtype}'
+
+        _x = np.random.uniform(0.01, 1.0, size=shape)
+
+        tdtype = getattr(torch, dtype.value)
+        tx = torch.tensor(_x, dtype=tdtype, requires_grad=True)
+        tx = torch.nn.Parameter(tx)
+        toptim = torch.optim.Adam([tx], lr=lr, betas=betas, eps=eps)
+
+        x = tensorgrad.Tensor(tx.detach().numpy(), name='x', backend=backend, dtype=dtype, requires_grad=True)
+        optim = tensorgrad.nn.Adam([x], lr=lr, betas=betas, eps=eps)
+
+        for _ in range(num_steps):
+            toptim.zero_grad()
+            tf = tx.exp().mean()
+            tf.backward()
+            toptim.step()
+
+            optim.zero_grad()
+            f = x.exp().mean()
+            f.backward()
+            optim.step()
             
             self.assertTrue(check_tensors(tf, f, tol=1e-5, show_diff=True), msg=f'forward@{name}')
             self.assertTrue(check_tensors(tx.grad, x.grad, tol=1e-5, show_diff=True), msg=f'x_grad@{name}')
