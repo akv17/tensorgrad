e831b2032e35cfdb8d69bfe2ee2621af1e33a3f0
akv17 <temakep@list.ru>
2023-08-31 23:04:38 +0300

    add full numpy broadcasting support for grad accum

--- tensorgrad/ops/util.py
+++ tensorgrad/ops/util.py
@@ -1,13 +1,29 @@
+import numpy as np
+
+
 def accumulate_broadcasted_grad(x, g):
-    """accumulates grad `g` of `x` over broadcast-inserted dims of `x` if `x` was broadcasted"""
+    """accumulates grad `g` of `x` over broadcasted dims of `x`"""
     g = g.copy()
     x_shape = x.shape
     g_shape = g.shape
-    if len(x_shape) >= len(g_shape):
+    if len(x_shape) > len(g_shape):
         return g
-    dim_diff = len(g_shape) - len(x_shape)
-    x_broad_dims = list(range(dim_diff))
-    while x_broad_dims:
-        dim = x_broad_dims.pop(-1)
-        g = g.sum(dim)
+    # right align dims following numpy broadcasting rules.
+    diff = len(g_shape) - len(x_shape)
+    x_dims = [None] * diff + list(range(len(x_shape)))
+    g_dims = list(range(len(g_shape)))
+    dims_zip = [*zip(x_dims, g_dims)]
+    for xd, gd in reversed(dims_zip):
+        # expanded dim.
+        if xd is None:
+            g = g.sum(gd)
+            continue
+        xs = x_shape[xd]
+        gs = g_shape[gd]
+        # dim of size one which was actually broadcasted.
+        # we need to keep it so expanding it back after reduce.
+        # this is essentially `keepdim=True` behavior.
+        if xs == 1 and gs != 1:  
+            g = g.sum(xd)
+            g = np.expand_dims(g, xd)
     return g
