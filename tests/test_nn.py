0116906e44f542b8f723628f28d79f20b6e28ebd
akv17 <temakep@list.ru>
2023-09-01 20:35:00 +0300

    use helper for mod tests

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -28,34 +28,16 @@ class TestNN(unittest.TestCase):
         [(128, 256), 512, True],
     ])
     def test_linear(self, shape, out_features, bias):
-        _x = np.random.normal(size=shape)
-        in_features = _x.shape[-1]
-        
-        tdtype = getattr(torch, DTYPE.value)
-        tx = torch.tensor(_x, dtype=tdtype, requires_grad=True)
-        tm = torch.nn.Linear(in_features=in_features, out_features=out_features, bias=bias)
-        to = tm(tx)
-        self.helper._backward_torch(to)
-
-        x = tensorgrad.Tensor(_x, dtype=DTYPE, device=DEVICE, name='x', requires_grad=True)
-        m = tensorgrad.nn.Linear(in_features=in_features, out_features=out_features, bias=bias)
-        m.weight = tensorgrad.Tensor(tm.weight.detach().numpy(), dtype=DTYPE, device=DEVICE, name='w', requires_grad=True)
-        if bias:
-            m.bias = tensorgrad.Tensor(tm.bias.detach().numpy(), dtype=DTYPE, device=DEVICE, name='b', requires_grad=True)
-        o = m(x)
-        self.helper._backward_tensorgrad(o)
-
-        tol = 1e-5
-        name = f'{shape}'
-        self.helper._check_tensors([
-            [to, o, tol, f'{name}@forward'],
-            [tx.grad, x.grad, tol, f'{name}@x_grad'],
-            [tm.weight.grad, m.weight.grad, tol, f'{name}@w_grad'],
-        ])
-        if bias:
-            self.helper._check_tensors([
-                [tm.bias.grad, m.bias.grad, tol, f'{name}@b_grad'],
-            ])
+        kwargs = {'in_features': shape[-1], 'out_features': out_features, 'bias': bias}
+        name = str(kwargs)
+        self.helper._test_module_with_weight_and_bias(
+            test_name=name,
+            module='Linear',
+            input_shape=shape,
+            torch_kwargs=kwargs,
+            tensorgrad_kwargs=kwargs,
+            tol=1e-5,
+        )
 
     @parameterized.expand([
         [2, (32, 32), (3, 3), 3, 16, False, 1, 0],
@@ -78,114 +60,96 @@ class TestNN(unittest.TestCase):
         stride,
         padding
     ):
-        _x = np.random.normal(size=(batch_size, in_channels, *input_size))
-        in_channels = in_channels
-        out_channels = out_channels
-
-        tdtype = getattr(torch, DTYPE.value)
-        tx = torch.tensor(_x, dtype=tdtype, requires_grad=True)
-        tm = torch.nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            bias=bias
+        shape = (batch_size, in_channels, *input_size)
+        kwargs = {
+            'in_channels': in_channels,
+            'out_channels': out_channels,
+            'kernel_size': kernel_size,
+            'stride': stride,
+            'padding': padding,
+            'bias': bias,
+        }
+        name = str(kwargs)
+        self.helper._test_module_with_weight_and_bias(
+            test_name=name,
+            module='Conv2d',
+            input_shape=shape,
+            torch_kwargs=kwargs,
+            tensorgrad_kwargs=kwargs,
+            tol=1e-4,
         )
-        to = tm(tx)
-        self.helper._backward_torch(to)
-
-        x = tensorgrad.Tensor(_x, device=DEVICE, dtype=DTYPE, requires_grad=True, name='x')
-        m = tensorgrad.nn.Conv2d(
-            in_channels=in_channels,
-            out_channels=out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            bias=bias
-        )
-        m.weight = tensorgrad.Tensor(tm.weight.detach().numpy(), dtype=DTYPE, device=DEVICE, name='w', requires_grad=True)
-        if bias:
-            m.bias = tensorgrad.Tensor(tm.bias.detach().numpy(), dtype=DTYPE, device=DEVICE, name='b', requires_grad=True)
-        o = m(x)
-        self.helper._backward_tensorgrad(o)
-
-        name = f'{batch_size}::{input_size}::{kernel_size}::{in_channels}::{out_channels}::{bias}::{stride}::{padding}'
-        tol = 1e-4
-        self.helper._check_tensors([
-            [to, o, tol, f'{name}@forward'],
-            [tx.grad, x.grad, tol, f'{name}@x_grad'],
-            [tm.weight.grad, m.weight.grad, tol, f'{name}@w_grad'],
-        ])
-        if bias:
-            self.helper._check_tensors([
-                [tm.bias.grad, m.bias.grad, tol, f'{name}@b_grad'],
-            ])
-
+    
     @parameterized.expand([
         [(2, 8)],
         [(16, 32)],
         [(128, 256)],
     ])
     def test_batch_norm1d(self, shape):
-        _x = np.random.normal(size=shape)
-        num_features = _x.shape[1]
-        
-        tdtype = getattr(torch, DTYPE.value)
-        tx = torch.tensor(_x, dtype=tdtype, requires_grad=True)
-        tm = torch.nn.BatchNorm1d(num_features=num_features)
-        to = tm(tx)
-        self.helper._backward_torch(to)
-
-        x = tensorgrad.Tensor(_x, dtype=DTYPE, device=DEVICE, name='x', requires_grad=True)
-        m = tensorgrad.nn.BatchNorm1d(num_features=num_features)
-        m.weight = tensorgrad.Tensor(tm.weight.detach().numpy(), dtype=DTYPE, device=DEVICE, name='w', requires_grad=True)
-        m.bias = tensorgrad.Tensor(tm.bias.detach().numpy(), dtype=DTYPE, device=DEVICE, name='b', requires_grad=True)
-        o = m(x)
-        self.helper._backward_tensorgrad(o)
-
-        tol = 1e-5
-        name = f'{shape}'
-        self.helper._check_tensors([
-            [to, o, tol, f'{name}@forward'],
-            [tx.grad, x.grad, tol, f'{name}@x_grad'],
-            [tm.weight.grad, m.weight.grad, tol, f'{name}@w_grad'],
-            [tm.bias.grad, m.bias.grad, tol, f'{name}@b_grad'],
-        ])
+        kwargs = {'num_features': shape[-1]}
+        name = str(kwargs)
+        self.helper._test_module_with_weight_and_bias(
+            test_name=name,
+            module='BatchNorm1d',
+            input_shape=shape,
+            torch_kwargs=kwargs,
+            tensorgrad_kwargs=kwargs,
+            tol=1e-4,
+        )
 
     @parameterized.expand([
         [(2, 3, 4, 4)],
         [(4, 8, 16, 16)],
-        [(8, 16, 64, 64)],
+        [(8, 16, 32, 32)],
     ])
     def test_batch_norm2d(self, shape):
-        _x = np.random.normal(size=shape)
-        num_features = _x.shape[1]
-
-        tdtype = getattr(torch, DTYPE.value)
-        tx = torch.tensor(_x, dtype=tdtype, requires_grad=True)
-        tm = torch.nn.BatchNorm2d(num_features=num_features)
-        to = tm(tx)
-        self.helper._backward_torch(to)
-
-        x = tensorgrad.Tensor(_x, dtype=DTYPE, device=DEVICE, name='x', requires_grad=True)
-        m = tensorgrad.nn.BatchNorm2d(num_features=num_features)
-        m.weight = tensorgrad.Tensor(tm.weight.detach().numpy(), dtype=DTYPE, device=DEVICE, name='w', requires_grad=True)
-        m.bias = tensorgrad.Tensor(tm.bias.detach().numpy(), dtype=DTYPE, device=DEVICE, name='b', requires_grad=True)
-        o = m(x)
-        self.helper._backward_tensorgrad(o)
-
-        tol = 1e-4
-        name = f'{shape}'
-        self.helper._check_tensors([
-            [to, o, tol, f'{name}@forward'],
-            [tx.grad, x.grad, tol, f'{name}@x_grad'],
-            [tm.weight.grad, m.weight.grad, tol, f'{name}@w_grad'],
-            [tm.bias.grad, m.bias.grad, tol, f'{name}@b_grad'],
-        ])
+        kwargs = {'num_features': shape[1]}
+        name = str(kwargs)
+        self.helper._test_module_with_weight_and_bias(
+            test_name=name,
+            module='BatchNorm2d',
+            input_shape=shape,
+            torch_kwargs=kwargs,
+            tensorgrad_kwargs=kwargs,
+            tol=1e-4,
+        )
 
 
 class Helper(unittest.TestCase):
+
+    def _test_module_with_weight_and_bias(
+        self,
+        test_name,
+        module,
+        input_shape,
+        torch_kwargs,
+        tensorgrad_kwargs,
+        tol=1e-5,
+    ):
+        _x = np.random.normal(size=input_shape)
+        
+        tdtype = getattr(torch, DTYPE.value)
+        tx = torch.tensor(_x, dtype=tdtype, requires_grad=True)
+        tm = getattr(torch.nn, module)(**torch_kwargs)
+        to = tm(tx)
+        self._backward_torch(to)
+
+        x = tensorgrad.Tensor(_x, dtype=DTYPE, device=DEVICE, name='x', requires_grad=True)
+        m = getattr(tensorgrad.nn, module)(**tensorgrad_kwargs)
+        m.weight = tensorgrad.Tensor(tm.weight.detach().numpy(), dtype=DTYPE, device=DEVICE, name='w', requires_grad=True)
+        if tm.bias is not None:
+            m.bias = tensorgrad.Tensor(tm.bias.detach().numpy(), dtype=DTYPE, device=DEVICE, name='b', requires_grad=True)
+        o = m(x)
+        self._backward_tensorgrad(o)
+
+        self._check_tensors([
+            [to, o, tol, f'{test_name}@forward'],
+            [tx.grad, x.grad, tol, f'{test_name}@x_grad'],
+            [tm.weight.grad, m.weight.grad, tol, f'{test_name}@w_grad'],
+        ])
+        if tm.bias is not None:
+            self._check_tensors([
+                [tm.bias.grad, m.bias.grad, tol, f'{test_name}@b_grad'],
+            ])
 
     def _check_tensors(self, pairs):
         for tt, t, tol, name in pairs:
