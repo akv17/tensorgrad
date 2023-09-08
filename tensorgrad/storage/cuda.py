3a01a04e28628bd35855fff963ec652dfdd307a0
akv17 <temakep@list.ru>
2023-09-08 23:31:40 +0300

    add numpy storage and impl cpu and cuda off it

--- tensorgrad/storage/cuda.py
+++ tensorgrad/storage/cuda.py
@@ -1,19 +1,8 @@
-from .cpu import CPUStorage as _NumpyStorage
+from .np import NumpyStorage
 from ..const import DEVICE
+from ..util.lazy import LazyImport
 
-_CUPY = None
 
-
-class CUDAStorage(_NumpyStorage):
-
-    @classmethod
-    def get_device(cls, data):
-        return DEVICE.CUDA
-
-    @classmethod
-    def _get_numpy(cls):
-        global _CUPY
-        if _CUPY is None:
-            import cupy
-            _CUPY = cupy
-        return _CUPY
+class CUDAStorage(NumpyStorage):
+    np = LazyImport('cupy', err_msg='Storage of CUDA device is not available')
+    DEVICE = DEVICE.CUDA
---
+++ tensorgrad/storage/np.py
@@ -0,0 +1,91 @@
+from ..const import DTYPE
+
+
+class NumpyStorage:
+    np = None
+    DEVICE = None
+
+    @classmethod
+    def tensor(cls, data, dtype=None):
+        dtype = cls._map_dtype(dtype)
+        data = cls.np.array(data, dtype=dtype)
+        return data
+    
+    @classmethod
+    def empty(cls, shape, dtype=None):
+        dtype = cls._map_dtype(dtype)
+        data = cls.np.empty(shape, dtype=dtype)
+        return data
+
+    @classmethod
+    def zeros(cls, shape, dtype=None):
+        dtype = cls._map_dtype(dtype)
+        data = cls.np.zeros(shape, dtype=dtype)
+        return data
+
+    @classmethod
+    def ones(cls, shape, dtype=None):
+        dtype = cls._map_dtype(dtype)
+        data = cls.np.ones(shape, dtype=dtype)
+        return data
+    
+    @classmethod
+    def arange(cls, n, dtype=None):
+        dtype = cls._map_dtype(dtype)
+        data = cls.np.arange(n, dtype=dtype)
+        return data
+
+    @classmethod
+    def random_uniform(cls, a, b, shape, dtype=None):
+        dtype = cls._map_dtype(dtype)
+        data = cls.np.random.uniform(a, b, size=shape).astype(dtype)
+        return data
+    
+    @classmethod
+    def bernoulli(cls, p, shape, dtype=None):
+        dtype = cls._map_dtype(dtype)
+        data = cls.np.random.binomial(n=1, p=p, size=shape).astype(dtype)
+        return data
+
+    @classmethod
+    def cast(cls, data, dtype):
+        dtype = cls._map_dtype(dtype)
+        data = data.astype(dtype)
+        return data
+
+    @classmethod
+    def get_device(cls, data):
+        return cls.DEVICE
+    
+    @classmethod
+    def get_dtype(cls, data):
+        return cls._imap_dtype(data.dtype)
+    
+    @classmethod
+    def get_shape(cls, data):
+        return data.shape
+
+    @classmethod
+    def _map_dtype(cls, dtype):
+        np = cls.np
+        dtype = dtype or DTYPE.FLOAT32
+        dispatch = {
+            DTYPE.FLOAT32: np.float32,
+            DTYPE.FLOAT64: np.float64,
+            DTYPE.INT32: np.int32,
+            DTYPE.INT64: np.int64,
+            DTYPE.BOOL: bool,
+        }
+        return dispatch[dtype]
+    
+    @classmethod
+    def _imap_dtype(cls, dtype):
+        dtype = str(dtype)
+        dispatch = {
+            'float32': DTYPE.FLOAT32,
+            'float64': DTYPE.FLOAT64,
+            'int32': DTYPE.INT32,
+            'int64': DTYPE.INT64,
+            'bool': DTYPE.BOOL,
+        }
+        return dispatch[dtype]
