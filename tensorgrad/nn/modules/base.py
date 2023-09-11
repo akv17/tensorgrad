7c2ec0b41ec6f38ea0cfc31f18a11ca3acc31939
akv17 <temakep@list.ru>
2023-09-11 16:46:39 +0300

    add buffer type

--- tensorgrad/nn/modules/base.py
+++ tensorgrad/nn/modules/base.py
@@ -9,13 +9,26 @@ class Parameter(Tensor):
         value = super().__repr__()
         value = value.replace('Tensor', 'Parameter')
         return value
+    
+
+class Buffer(Tensor):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.requires_grad = False
+
+    def __repr__(self):
+        value = super().__repr__()
+        value = value.replace('Tensor', 'Buffer')
+        return value
 
 
 class Module(ABC):
 
     def __init__(self):
-        self._parameters = {}
         self._modules = {}
+        self._parameters = {}
+        self._buffers = {}
         self.training = False
 
     def __setattr__(self, name, value):
@@ -23,6 +36,8 @@ class Module(ABC):
             self._parameters[name] = value
         elif isinstance(value, Module):
             self._modules[name] = value
+        elif isinstance(value, Buffer):
+            self._buffers[name] = value
         super().__setattr__(name, value)
 
     def __call__(self, *args, **kwargs):
@@ -34,10 +49,10 @@ class Module(ABC):
     def forward(self, *args, **kwargs): pass
 
     def to(self, device):
-        for pn, p in self._parameters.items():
-            p = p.to(device)
-            self._parameters[pn] = p
-            setattr(self, pn, p)
+        params_and_buffers = {**self._parameters, **self._buffers}
+        for k, v in params_and_buffers.items():
+            v = v.to(device)
+            setattr(self, k, v)
         for m in self._modules.values():
             m.to(device)
 
@@ -53,21 +68,16 @@ class Module(ABC):
 
     def init_from_torch(self, module):
         pass
-
-    def parameters(self):
-        return list(self.named_parameters().values())
     
     def modules(self):
         return list(self.named_modules().values())
-
-    def named_parameters(self):
-        kv = {**self._parameters}
-        for mn, m in self._modules.items():
-            for pn, p in m.named_parameters().items():
-                pk = f'{mn}.{pn}'
-                kv[pk] = p
-        return kv
-
+    
+    def parameters(self):
+        return list(self.named_parameters().values())
+    
+    def buffers(self):
+        return list(self.named_buffers().values())
+    
     def named_modules(self):
         # prefix 's' stands for 'sub' -> 'submodule'.
         kv = {}
@@ -76,6 +86,25 @@ class Module(ABC):
             for smn, sm in m.named_modules().items():
                 smk = f'{mn}.{smn}'
                 kv[smk] = sm
+        return kv
+
+    def named_parameters(self):
+        kv = self._named_params_and_buffers()['params']
+        return kv
+    
+    def named_buffers(self):
+        kv = self._named_params_and_buffers()['buffers']
+        return kv
+
+    def _named_params_and_buffers(self):
+        kv = {'params': self._parameters.copy(), 'buffers': self._buffers.copy()}
+        for mn, m in self._modules.items():
+            for pn, p in m.named_parameters().items():
+                pk = f'{mn}.{pn}'
+                kv['params'][pk] = p
+            for bn, b in m.named_buffers().items():
+                bk = f'{mn}.{bn}'
+                kv['buffers'][bk] = b
         return kv
     
     def _check_input(self, *args, **kwargs): pass
